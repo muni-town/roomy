500a44ac857e99efd7a0d0f56071ef5cf8cd60ac
ibro <sameoldlab@proton.me>
2025-03-10 14:38:16 +0100

    open url in browser

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,8 @@
+use tauri::{command, Builder};
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-  tauri::Builder::default()
+  Builder::default()
     .setup(|app| {
       if cfg!(debug_assertions) {
         app.handle().plugin(
@@ -11,6 +13,32 @@ pub fn run() {
       }
       Ok(())
     })
+    .invoke_handler(tauri::generate_handler![open_url])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
+}
+
+
+#[command(rename_all = "snake_case")]
+fn open_url(url: &str) -> Result<(), Error> {
+  open::that(url).unwrap();
+
+  Ok(())
+}
+
+
+#[derive(Debug, thiserror::Error)]
+enum Error {
+  #[error(transparent)]
+  Io(#[from] std::io::Error)
+}
+
+// we must manually implement serde::Serialize
+impl serde::Serialize for Error {
+  fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+  where
+    S: serde::ser::Serializer,
+  {
+    serializer.serialize_str(self.to_string().as_ref())
+  }
 }
