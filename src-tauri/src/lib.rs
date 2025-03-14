3337094946f4daa981b64151b8bc5b24da12e75a
ibro <sameoldlab@proton.me>
2025-03-14 17:58:00 +0100

    deep-link debug config

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,23 +1,44 @@
-use tauri::{command, Builder};
+use tauri::{command, Builder, Manager};
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-  Builder::default()
-    .setup(|app| {
-      if cfg!(debug_assertions) {
-        app.handle().plugin(
-          tauri_plugin_log::Builder::default()
-            .level(log::LevelFilter::Info)
-            .build(),
-        )?;
-      }
-      Ok(())
-    })
-    .invoke_handler(tauri::generate_handler![open_url])
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
-}
+    let mut builder = Builder::default();
+    #[cfg(desktop)]
+    {
+        builder = builder.plugin(tauri_plugin_single_instance::init(|app, args, _cwd| {
+          println!("a new app instance was opened with {args:?} and the deep link event was already triggered");
+          // focus running instance when new app instance is requested
+          let _ = app.get_webview_window("main")
+           .expect("no main window")
+           .set_focus();
+        }));
+    }
+    builder
+        .plugin(tauri_plugin_deep_link::init())
+        .setup(|app| {
+            #[cfg(any(target_os = "linux", windows))]
+            {
+                use tauri_plugin_deep_link::DeepLinkExt;
+                app.deep_link().register_all()?;
+                app.deep_link().on_open_url(|event| {
+                    println!("deep link URLs: {:?}", event.urls());
+                });
+            }
 
+            #[cfg(debug_assertions)]
+            {
+                app.handle().plugin(
+                    tauri_plugin_log::Builder::default()
+                        .level(log::LevelFilter::Info)
+                        .build(),
+                )?;
+            }
+            Ok(())
+        })
+        .invoke_handler(tauri::generate_handler![open_url])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
 
 #[command(rename_all = "snake_case")]
 fn open_url(url: &str) -> Result<(), Error> {
