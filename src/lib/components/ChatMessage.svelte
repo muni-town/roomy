2a412c18b762dbf6069329f7dad984d8f4eb442f
Justin Kimlim <kimlimjustin@gmail.com>
2025-03-15 17:38:13 +0800

    feat: detailed timestamp on hover

--- src/lib/components/ChatMessage.svelte
+++ src/lib/components/ChatMessage.svelte
@@ -298,7 +298,7 @@
   {#await authorProfile then authorProfile}
     {@render toolbar(authorProfile)}
 
-    <div class="flex gap-4">
+    <div class="flex gap-4 message-container">
       {#if shouldShowAuthor}
         <a
           href={`https://bsky.app/profile/${authorProfile.handle}`}
@@ -311,7 +311,12 @@
           />
         </a>
       {:else}
-        <div class="w-8.5"></div>
+        <div class="w-8.5 timestamp-placeholder">
+          <span class="timestamp-text"
+            >{message.createdDate &&
+              format(message.createdDate, "HH:mm:ss")}</span
+          >
+        </div>
       {/if}
 
       <Button.Root
@@ -541,3 +546,28 @@
     {/if}
   {/await}
 {/snippet}
+
+<style>
+  .timestamp-placeholder {
+    position: relative;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .timestamp-text {
+    opacity: 0;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    font-size: 0.75rem;
+    color: rgb(209, 213, 219);
+    transition: opacity 0.2s ease-in-out;
+    white-space: nowrap;
+  }
+
+  .message-container:hover .timestamp-text {
+    opacity: 1;
+  }
+</style>
