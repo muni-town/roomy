1d591f640c3a49a8e3051ba478216f597248e8e7
Michael Schultz <michaelwschultz@gmail.com>
2025-04-16 23:09:15 -0700

    Adds posthog analytics initialization in prod

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
   import "../../app.css";
   import { onMount } from "svelte";
-  import { dev } from "$app/environment";
+  import posthog from "posthog-js";
+
+  import { browser, dev } from "$app/environment";
   import { g } from "$lib/global.svelte";
   import { user } from "$lib/user.svelte";
   import { cleanHandle, derivePromise, navigate } from "$lib/utils.svelte";
@@ -12,7 +14,7 @@
 
   import { Toaster } from "svelte-french-toast";
   import { RenderScan } from "svelte-render-scan";
-  import { Button, ToggleGroup, Avatar } from "bits-ui";
+  import { Button, ToggleGroup } from "bits-ui";
 
   import ThemeSelector from "$lib/components/ThemeSelector.svelte";
   import { Space } from "@roomy-chat/sdk";
@@ -34,6 +36,13 @@
 
   onMount(async () => {
     await user.init();
+
+    if (!dev && browser) {
+      posthog.init("phc_j80ksIuoxjfjRI7rPBmTLWx79rntg4Njz6Dixc3I3ik", {
+        api_host: "https://us.i.posthog.com",
+        person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
+      });
+    }
   });
 
   $effect(() => {
@@ -44,7 +53,7 @@
     if (!newSpaceName || !user.agent || !g.roomy) return;
     const space = await g.roomy.create(Space);
     space.name = newSpaceName;
-    space.admins((x) => x.push(user.agent!.assertDid));
+    space.admins((x) => user.agent && x.push(user.agent.assertDid));
     space.commit();
 
     g.roomy.spaces.push(space);
