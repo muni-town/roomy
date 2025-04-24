eef6d61655d9d4860f3a758e20a2af194ed403f0
Michael Schultz <michaelwschultz@gmail.com>
2025-04-24 14:03:24 -0700

    remove some deps and move others to dev

--- svelte.config.js
+++ svelte.config.js
@@ -2,10 +2,7 @@ import adapter from "@sveltejs/adapter-netlify";
 import adapterStatic from "@sveltejs/adapter-static";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://svelte.dev/docs/kit/integrations
-  // for more information about preprocessors
   preprocess: vitePreprocess(),
 
   kit: {
@@ -17,7 +14,9 @@ const config = {
         ? adapterStatic({
             fallback: "index.html",
           })
-        : adapter(),
+        : adapter({
+            fallback: "index.html",
+          }),
   },
 };
 
