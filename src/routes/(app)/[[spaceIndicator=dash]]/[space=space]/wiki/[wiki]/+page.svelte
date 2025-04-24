7201f858a463578d57c495f3f7b1e479840577c9
ibro <sameoldlab@proton.me>
2025-04-24 14:04:43 +0100

    display wikiEditor as page

--- src/routes/(app)/[[spaceIndicator=dash]]/[space=space]/wiki/[wiki]/+page.svelte
+++ src/routes/(app)/[[spaceIndicator=dash]]/[space=space]/wiki/[wiki]/+page.svelte
@@ -1,6 +1,18 @@
 <script lang="ts">
   import { page } from "$app/state";
+  import WikiEditor from "$lib/components/WikiEditor.svelte";
+  import { g } from "$lib/global.svelte";
+  import { derivePromise } from "$lib/utils.svelte";
+  import { Channel, WikiPage } from "@roomy-chat/sdk";
+
+  const wiki = derivePromise(
+    null,
+    async () => await g.space?.wikipages.open(WikiPage, page.params.wiki),
+  );
 </script>
 
-{page.params.wiki}
-{page.params.space}
+{#if wiki.value}
+  <div class="w-full">
+    <WikiEditor wiki={wiki.value} />
+  </div>
+{/if}
