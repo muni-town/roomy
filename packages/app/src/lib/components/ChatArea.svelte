09d795924869fd22c1b37ca473b0d3845d59da6b
ibro <sameoldlab@proton.me>
2025-05-09 14:54:25 +0100

    fix merge

--- packages/app/src/lib/components/ChatArea.svelte
+++ packages/app/src/lib/components/ChatArea.svelte
@@ -12,6 +12,7 @@
   import { derivePromise } from "$lib/utils.svelte";
   import { page } from "$app/state";
   import LinkPreview from "./LinkPreview.svelte";
+  import { globalState } from "$lib/global.svelte";
 
   let {
     timeline,
@@ -123,15 +124,17 @@
               {@const previousMessage =
                 index > 0 ? messages.value[index - 1] : undefined}
               {#if !message.softDeleted}
-                <ChatMessage
-                  {message}
-                  mergeWithPrevious={shouldMergeWithPrevious(
-                    message,
-                    previousMessage,
-                  )}
-                />
-              {:else if "bodyJson" in message}
-                <LinkPreview {message} />
+                {#if globalState.channel?.name !== "@links"}
+                  <ChatMessage
+                    {message}
+                    mergeWithPrevious={shouldMergeWithPrevious(
+                      message,
+                      previousMessage,
+                    )}
+                  />
+                {:else if "bodyJson" in message}
+                  <LinkPreview {message} />
+                {/if}
               {:else}
                 <p class="italic text-error text-sm">
                   This message has been deleted
