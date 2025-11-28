f5e4690da59cc564da83d2ae4907f45e749e13b6
meri-leeworthy <meri.leeworthy@gmail.com>
2025-11-28 11:51:47 +1100

    chore: delete old jazz sdk

--- packages/sdk/src/functions/group.ts
+++
@@ -1,8 +0,0 @@
-import { Group } from "jazz-tools";
-
-export function publicGroup(readWrite: "reader" | "writer" = "reader") {
-  const group = Group.create();
-  group.addMember("everyone", readWrite);
-
-  return group;
-}
