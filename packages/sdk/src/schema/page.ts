1a6a913773ab1c22691978c75e3ea8c92668ec8e
meri-leeworthy <meri.leeworthy@gmail.com>
2025-11-28 11:51:47 +1100

    chore: delete old jazz sdk

--- packages/sdk/src/schema/page.ts
+++
@@ -1,8 +0,0 @@
-import { co, z } from "jazz-tools";
-import { defComponent } from "./roomyentity.js";
-
-export const PageContent = co.map({
-  text: z.string(),
-});
-
-export const PageComponent = defComponent("space.roomy.page.v0", PageContent);
