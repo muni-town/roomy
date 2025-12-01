447a5fd6b8825e31f611435cdb441f55346904fb
meri-leeworthy <meri.leeworthy@gmail.com>
2025-12-01 17:30:59 +1100

    fix: use 'next' oauth scopes on next

--- packages/app/scripts/build-prod.sh
+++ packages/app/scripts/build-prod.sh
@@ -8,6 +8,13 @@ echo "Generating OAuth client configuration..."
 echo "OAuth Host URL: $OAUTH_HOST"
 echo "Target URL: $target_url"
 
+# Determine which repo scopes to use based on OAUTH_HOST
+if [[ "$OAUTH_HOST" == "https://next.roomy.space" ]]; then
+  repo_scopes="repo:space.roomy.upload repo:space.roomy.stream.next repo:space.roomy.stream.handle.next"
+else
+  repo_scopes="repo:space.roomy.upload repo:space.roomy.stream repo:space.roomy.stream.handle"
+fi
+
 # Add oauth-client configuration
 oauth_config="{
   \"client_id\": \"$target_url/oauth-client-metadata.json\",
@@ -17,7 +24,7 @@ oauth_config="{
   \"tos_uri\": \"$target_url\",
   \"policy_uri\": \"$target_url\",
   \"redirect_uris\": [\"$target_url/oauth/callback\"],
-  \"scope\": \"atproto blob:*/* repo:space.roomy.upload rpc:app.bsky.actor.getProfile?aud=did:web:api.bsky.app%23bsky_appview rpc:chat.bsky.actor.deleteAccount?aud=did:web:api.bsky.chat%23bsky_chat rpc:chat.bsky.actor.exportAccountData?aud=did:web:api.bsky.chat%23bsky_chat rpc:chat.bsky.convo.acceptConvo?aud=did:web:api.bsky.chat%23bsky_chat rpc:chat.bsky.convo.deleteMessageForSelf?aud=did:web:api.bsky.chat%23bsky_chat rpc:chat.bsky.convo.getConvoAvailability?aud=did:web:api.bsky.chat%23bsky_chat rpc:chat.bsky.convo.getConvoForMembers?aud=did:web:api.bsky.chat%23bsky_chat rpc:chat.bsky.convo.getConvo?aud=did:web:api.bsky.chat%23bsky_chat rpc:chat.bsky.convo.getLog?aud=did:web:api.bsky.chat%23bsky_chat rpc:chat.bsky.convo.leaveConvo?aud=did:web:api.bsky.chat%23bsky_chat rpc:chat.bsky.convo.listConvos?aud=did:web:api.bsky.chat%23bsky_chat rpc:chat.bsky.convo.muteConvo?aud=did:web:api.bsky.chat%23bsky_chat rpc:chat.bsky.convo.removeReaction?aud=did:web:api.bsky.chat%23bsky_chat rpc:chat.bsky.convo.sendMessageBatch?aud=did:web:api.bsky.chat%23bsky_chat rpc:chat.bsky.convo.unmuteConvo?aud=did:web:api.bsky.chat%23bsky_chat rpc:chat.bsky.convo.addReaction?aud=did:web:api.bsky.chat%23bsky_chat rpc:chat.bsky.convo.updateAllRead?aud=did:web:api.bsky.chat%23bsky_chat rpc:chat.bsky.convo.updateRead?aud=did:web:api.bsky.chat%23bsky_chat rpc:chat.bsky.moderation.getActorMetadata?aud=did:web:api.bsky.chat%23bsky_chat rpc:chat.bsky.moderation.getMessageContext?aud=did:web:api.bsky.chat%23bsky_chat rpc:chat.bsky.moderation.updateActorAccess?aud=did:web:api.bsky.chat%23bsky_chat repo:space.roomy.stream repo:space.roomy.stream.handle rpc:town.muni.leaf.authenticate?aud=*\",
+  \"scope\": \"atproto blob:*/* $repo_scopes repo:space.roomy.upload rpc:app.bsky.actor.getProfile?aud=did:web:api.bsky.app%23bsky_appview rpc:chat.bsky.actor.deleteAccount?aud=did:web:api.bsky.chat%23bsky_chat rpc:chat.bsky.actor.exportAccountData?aud=did:web:api.bsky.chat%23bsky_chat rpc:chat.bsky.convo.acceptConvo?aud=did:web:api.bsky.chat%23bsky_chat rpc:chat.bsky.convo.deleteMessageForSelf?aud=did:web:api.bsky.chat%23bsky_chat rpc:chat.bsky.convo.getConvoAvailability?aud=did:web:api.bsky.chat%23bsky_chat rpc:chat.bsky.convo.getConvoForMembers?aud=did:web:api.bsky.chat%23bsky_chat rpc:chat.bsky.convo.getConvo?aud=did:web:api.bsky.chat%23bsky_chat rpc:chat.bsky.convo.getLog?aud=did:web:api.bsky.chat%23bsky_chat rpc:chat.bsky.convo.leaveConvo?aud=did:web:api.bsky.chat%23bsky_chat rpc:chat.bsky.convo.listConvos?aud=did:web:api.bsky.chat%23bsky_chat rpc:chat.bsky.convo.muteConvo?aud=did:web:api.bsky.chat%23bsky_chat rpc:chat.bsky.convo.removeReaction?aud=did:web:api.bsky.chat%23bsky_chat rpc:chat.bsky.convo.sendMessageBatch?aud=did:web:api.bsky.chat%23bsky_chat rpc:chat.bsky.convo.unmuteConvo?aud=did:web:api.bsky.chat%23bsky_chat rpc:chat.bsky.convo.addReaction?aud=did:web:api.bsky.chat%23bsky_chat rpc:chat.bsky.convo.updateAllRead?aud=did:web:api.bsky.chat%23bsky_chat rpc:chat.bsky.convo.updateRead?aud=did:web:api.bsky.chat%23bsky_chat rpc:chat.bsky.moderation.getActorMetadata?aud=did:web:api.bsky.chat%23bsky_chat rpc:chat.bsky.moderation.getMessageContext?aud=did:web:api.bsky.chat%23bsky_chat rpc:chat.bsky.moderation.updateActorAccess?aud=did:web:api.bsky.chat%23bsky_chat rpc:town.muni.leaf.authenticate?aud=*\",
   \"grant_types\": [\"authorization_code\", \"refresh_token\"],
   \"response_types\": [\"code\"],
   \"token_endpoint_auth_method\": \"none\",
