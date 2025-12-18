5af293f00083356783fae05b687e03aba02ca231
meri-leeworthy <meri.leeworthy@gmail.com>
2025-12-18 19:01:31 +1100

    iterating on leaf client api

--- packages/app/src/lib/workers/backend/modules.ts
+++ packages/app/src/lib/workers/backend/modules.ts
@@ -1,65 +1,69 @@
 import { LeafClient } from "@muni-town/leaf-client";
 
-export const personalModule = LeafClient.encodeBasicModule({
-  init_sql: `
+export const personalModuleDef = {
+  $type: "space.roomy.module.basic.0" as const,
+  def: {
+    init_sql: `
     create table if not exists stream_info as select
       (select creator from stream) as creator, 
       'space.roomy.stream.personal' as type,
       '2' as schema_version;
   `,
-  authorizer: `
+    authorizer: `
     select unauthorized("Only stream owner can add events")
       where (select creator from stream_info) != (select user from event);
   `,
-  materializer: ``,
-  queries: [
-    {
-      name: "stream_info",
-      sql: `select * from stream_info;`,
-      limits: [],
-      params: [],
-    },
-    {
-      name: "events",
-      sql: `
-        select unauthorized('only the strema creator can read its events')
+    materializer: ``,
+    queries: [
+      {
+        name: "stream_info",
+        sql: `select * from stream_info;`,
+        params: [],
+      },
+      {
+        name: "events",
+        sql: `
+        select unauthorized('only the stream creator can read its events')
           where $requesting_user != (select creator from stream_info);
 
         select id, user, payload from events.events
           where id >= $start limit $limit;
       `,
-      limits: [],
-      params: [],
-    },
-  ],
-});
-console.info("Personal module ID:", personalModule.moduleId);
+        params: [],
+      },
+    ],
+  },
+};
+export const personalModule = await LeafClient.encodeModule(personalModuleDef);
+console.info("Personal module ID:", personalModule.moduleCid);
 
-export const spaceModule = LeafClient.encodeBasicModule({
-  init_sql: `
+export const spaceModuleDef = {
+  $type: "space.roomy.module.basic.0" as const,
+  def: {
+    init_sql: `
     create table if not exists stream_info as select
       (select creator from stream) as creator, 
       'space.roomy.stream.space' as type,
       '2' as schema_version;
   `,
-  authorizer: ``,
-  materializer: ``,
-  queries: [
-    {
-      name: "stream_info",
-      sql: `select * from stream_info;`,
-      limits: [],
-      params: [],
-    },
-    {
-      name: "events",
-      sql: `
+    authorizer: ``,
+    materializer: ``,
+    queries: [
+      {
+        name: "stream_info",
+        sql: `select * from stream_info;`,
+        params: [],
+      },
+      {
+        name: "events",
+        sql: `
         select id, user, payload from events.events
           where id >= $start limit $limit;
       `,
-      limits: [],
-      params: [],
-    },
-  ],
-});
-console.info("Space module ID:", spaceModule.moduleId);
+        params: [],
+      },
+    ],
+  },
+};
+export const spaceModule = await LeafClient.encodeModule(spaceModuleDef);
+console.info("Space module ID:", spaceModule.moduleCid);
