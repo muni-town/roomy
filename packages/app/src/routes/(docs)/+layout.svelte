28cc341695eee5d1315ee2bafedeb8990e08a0e7
Zicklag <zicklag@katharostech.com>
2026-01-03 09:37:20 -0600

    feat: add a terms of service page.

---
+++ packages/app/src/routes/(docs)/+layout.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import "../../app.css";
+  import { dev } from "$app/environment";
+  import Navbar from "$lib/components/layout/Navbar.svelte";
+  import { Button, Prose, ScrollArea } from "@fuxui/base";
+
+  let { children } = $props();
+</script>
+
+<svelte:head>
+  {#if dev}
+    <!-- replaces favicon on dev, to easily spot difference between deployed and dev versions -->
+    <link
+      rel="icon"
+      href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👷‍♀️</text></svg>"
+    />
+  {/if}
+</svelte:head>
+
+<div class="flex flex-col items-center justify-stretch max-h-[100vh]">
+  <Navbar class="gap-2">
+    <h1 class="text-xl font-bold">Roomy Space Terms of Service</h1>
+    <span class="flex-grow"></span>
+    <Button href="/">App</Button>
+    <Button href="https://a.roomy.space">About</Button>
+  </Navbar>
+  <ScrollArea class="w-full flex flex-col items-center">
+    <Prose class="max-w-[50em] my-10 px-5">
+      {@render children()}
+    </Prose>
+  </ScrollArea>
+</div>
---
+++ packages/app/src/routes/(docs)/tos/+page.svelte
@@ -0,0 +1,357 @@
+<h3 id="important-terms">Important Terms</h3>
+<p>
+  These terms include a number of important provisions that affect your rights
+  and responsibilities, such as the disclaimers in Disclaimers, limits on the
+  company’s liability to you in Limits on Liability, your agreement to cover the
+  company for damages caused by your misuse of the platform in Responsibility
+  for Your Use, and an agreement to arbitrate disputes in Disputes.
+</p>
+<h3 id="your-permission-to-use-the-platform">
+  Your Permission to Use the Platform
+</h3>
+<p>
+  Subject to these terms, the company gives you permission to use the platform.
+  Everyone needs to agree to these terms to use the platform.
+</p>
+<h3 id="conditions-for-use-of-the-platform">
+  Conditions for Use of the Platform
+</h3>
+<p>
+  Your permission to use the platform is subject to the following conditions:
+</p>
+<ul>
+  <li>You must be at least thirteen years old.</li>
+  <li>
+    You may no longer use the platform if the company contacts you directly to
+    say that you may not.
+  </li>
+  <li>
+    You must use the platform in accordance with Acceptable Use and Content
+    Standards.
+  </li>
+</ul>
+<h3 id="acceptable-use">Acceptable Use</h3>
+<ul>
+  <li>You may not break the law using the platform.</li>
+  <li>
+    You may not use or try to use another’s account on the platform without
+    their specific permission.
+  </li>
+  <li>
+    You may not buy, sell, or otherwise trade in user names or other unique
+    identifiers on the platform.
+  </li>
+  <li>
+    You may not send advertisements, chain letters, or other solicitations
+    through the platform, or use the platform to gather addresses or other
+    personal data for commercial mailing lists or databases.
+  </li>
+  <li>
+    You may not automate access to the platform, or monitor the platform, such
+    as with a web crawler, browser plug-in or add-on, or other computer program
+    that is not a web browser. You may crawl the platform to index it for a
+    publicly available search engine, if you run one.
+  </li>
+  <li>
+    You may not use the platform to send e-mail to distribution lists,
+    newsgroups, or group mail aliases.
+  </li>
+  <li>
+    You may not falsely imply that you’re affiliated with or endorsed by the
+    company.
+  </li>
+  <li>
+    You may not hyperlink to images or other non-hypertext content on the
+    platform on other webpages.
+  </li>
+  <li>
+    You may not remove any marks showing proprietary ownership from materials
+    you download from the platform.
+  </li>
+  <li>
+    You may not show any part of the platform on other websites with <code
+      >&lt;iframe&gt;</code
+    >.
+  </li>
+  <li>
+    You may not disable, avoid, or circumvent any security or access
+    restrictions of the platform.
+  </li>
+  <li>
+    You may not strain infrastructure of the platform with an unreasonable
+    volume of requests, or requests designed to impose an unreasonable load on
+    information systems underlying the platform.
+  </li>
+  <li>You may not impersonate others through the platform.</li>
+  <li>You may not encourage or help anyone in violation of these terms.</li>
+</ul>
+<h3 id="content-standards">Content Standards</h3>
+<ul>
+  <li>
+    You may not submit content to the platform disruptive to civilized
+    discourse.
+  </li>
+  <li>
+    You may not submit content to the platform that is illegal, offensive, or
+    otherwise harmful to others. This includes content that is harassing,
+    inappropriate, or abusive.
+  </li>
+  <li>
+    You may not submit content to the platform that violates the law, infringes
+    anyone’s intellectual property rights, violates anyone’s privacy, or
+    breaches agreements you have with others.
+  </li>
+  <li>
+    You may not submit content to the platform containing malicious computer
+    code, such as computer viruses or spyware.
+  </li>
+  <li>
+    You may not submit content to the platform as a mere placeholder, to hold a
+    particular address, user name, or other unique identifier.
+  </li>
+  <li>
+    You may not use the platform to disclose information that you don’t have the
+    right to disclose, like others’ confidential or personal information.
+  </li>
+</ul>
+<h3 id="enforcement">Enforcement</h3>
+<p>
+  The company may investigate and prosecute violations of these terms to the
+  fullest legal extent. The company may notify and cooperate with law
+  enforcement authorities in prosecuting violations of the law and these terms.
+</p>
+<p>
+  The company reserves the right to change, redact, and delete content on the
+  platform for any reason. If you believe someone has submitted content to the
+  platform in violation of the law or these terms, flag the post for moderator
+  attention (NOTE: Feature pending) if you have an account or <a
+    href="https://a.roomy.space/contact">e-mail us immediately</a
+  >.
+</p>
+<h3 id="minimizing-exposure-and-content-removal-process">
+  Minimizing Exposure and Content Removal Process
+</h3>
+<p>The company aims to minimize the presence of illegal content by:</p>
+<ul>
+  <li>Prompt removal of clearly identified content upon detection</li>
+  <li>Temporary restrictions on flagged content while under review</li>
+  <li>
+    Notifying users when their content is removed for violating legal
+    requirements
+  </li>
+</ul>
+<p>
+  If illegal content is identified, the company will act as quickly as possible
+  to remove it. The company reserves the right to suspend or terminate accounts
+  associated with repeated violations.
+</p>
+<h3 id="moderation">Moderation</h3>
+<p>
+  The company may moderate the platform according to its moderation policy
+  (NOTE: Link pending).
+</p>
+<p>
+  The company may use all of the moderation tools of the platform’s software.
+  Those tools are described at (NOTE: Link pending)
+</p>
+<p>
+  The company may use behavioral analysis to detect suspicious activity and
+  prevent abuse.
+</p>
+<p>
+  Some moderation is done automatically and the company may use AI Triaging for
+  moderation. For information on automatic moderation, see (NOTE: Link pending)
+  in our privacy notice.
+</p>
+<p>
+  All automated actions are reversible and subject to human review where
+  necessary.
+</p>
+<p>
+  The company may restrict, suspend, or close your account on the platform
+  according to its moderation policy.
+</p>
+<h3 id="recommendations">Recommendations</h3>
+<p>
+  The platform automatically recommends topics to you in a number of ways. The
+  data used to make those recommendations, as well as your options affecting
+  them, are described at (NOTE: Link pending).
+</p>
+<h3 id="your-account">Your Account</h3>
+<p>
+  You must create and log into an account to use some features of the platform.
+</p>
+<p>
+  To create an account, you must provide some information about yourself. If you
+  create an account, you agree to provide, at a minimum, a valid e-mail address
+  (NOTE: Change pending), and to keep that address up-to-date. You may close
+  your account at any time by e-mailing team@roomy.space
+</p>
+<p>
+  You agree to be responsible for all action taken using your account, whether
+  authorized by you or not, until you either close your account or notify the
+  company that your account has been compromised. You agree to notify the
+  company immediately if you suspect your account has been compromised. You
+  agree to select a secure password for your account, and keep it secret.
+</p>
+<h3 id="your-content">Your Content</h3>
+<p>
+  Nothing in these terms gives the company any ownership rights in intellectual
+  property that you share with the platform, such as your account information,
+  posts, or other content you submit to the platform. Nothing in these terms
+  gives you any ownership rights in the company’s intellectual property, either.
+</p>
+<p>
+  Between you and the company, you remain solely responsible for content you
+  submit to the platform. You agree not to wrongly imply that content you submit
+  to the platform is sponsored or approved by the company. These terms do not
+  obligate the company to store, maintain, or provide copies of content you
+  submit.
+</p>
+<p>
+  Content you submit to the platform belongs to you, and you decide what
+  permission to give others for it. But at a minimum, you license the company to
+  provide content that you submit to the platform to other users of the
+  platform, and to process it, according to these terms. That special license
+  allows the company to copy, publish, and analyze content you submit to the
+  platform.
+</p>
+<p>
+  When content you submit is removed from the platform, whether by you or by the
+  company, the company’s special license ends when the last copy disappears from
+  the company’s backups, caches, and other systems. Other licenses you apply to
+  content you submit, such as Creative Commons licenses, may continue after your
+  content is removed. Those licenses may give others, or the company itself, the
+  right to share your content through the platform again.
+</p>
+<p>
+  Others who receive content you submit to the platform may violate the terms on
+  which you license your content. You agree that the company will not be liable
+  to you for those violations or their consequences.
+</p>
+<h3 id="your-responsibility">Your Responsibility</h3>
+<p>
+  You agree to indemnify the company from legal claims by others related to your
+  breach of these terms, or breach of these terms by others using your account
+  on the platform. Both you and the company agree to notify the other side of
+  any legal claims for which you might have to indemnify the company as soon as
+  possible. If the company fails to notify you of a legal claim promptly, you
+  won’t have to indemnify the company for damages that you could have defended
+  against or mitigated with prompt notice. You agree to allow the company to
+  control investigation, defense, and settlement of legal claims for which you
+  would have to indemnify the company, and to cooperate with those efforts. The
+  company agrees not to agree to any settlement that admits fault for you or
+  imposes obligations on you without your prior agreement.
+</p>
+<h3 id="disclaimers">Disclaimers</h3>
+<p>
+  You accept all risk of using the platform and content on the platform. As far
+  as the law allows, the company and its suppliers provide the platform as is,
+  without any warranty whatsoever.
+</p>
+<p>
+  The platform may hyperlink to and integrate platforms and services run by
+  others. The company does not make any warranty about services run by others,
+  or content they may provide. Use of services run by others may be governed by
+  other terms between you and the one running the service.
+</p>
+<h3 id="limits-on-liability">Limits on Liability</h3>
+<p>
+  Neither the company nor its suppliers will be liable to you for
+  breach-of-contract damages their personnel could not have reasonably foreseen
+  when you agreed to these terms.
+</p>
+<p>
+  As far as the law allows, the total liability to you for claims of any kind
+  that are related to the platform or content on the platform will be limited to
+  $50.
+</p>
+<h3 id="feedback">Feedback</h3>
+<p>
+  The company welcomes your feedback and suggestions for the platform. See the
+  Contact section below for ways to get in touch with us.
+</p>
+<p>
+  You agree that the company will be free to act on feedback and suggestions you
+  provide, and that the company won’t have to notify you that your feedback was
+  used, get your permission to use it, or pay you. You agree not to submit
+  feedback or suggestions that you believe might be confidential or proprietary,
+  to you or others.
+</p>
+<h3 id="termination">Termination</h3>
+<p>
+  Either you or the company may end the agreement written out in these terms at
+  any time. When our agreement ends, your permission to use the platform also
+  ends.
+</p>
+<p>
+  The following provisions continue after our agreement ends: Your Content,
+  Feedback, Your Responsibility, Disclaimers, Limits on Liability, and General
+  Terms.
+</p>
+<h3 id="disputes">Disputes</h3>
+<p>
+  Norwegian law will govern any dispute related to these terms or your use of
+  the platform.
+</p>
+<p>
+  You and the company agree to seek injunctions related to these terms only in
+  federal court in Oslo, Norway. Neither you nor the company will object to
+  jurisdiction, platform, or venue in those courts.
+</p>
+<p>
+  (NOTE: Pending section on Arbitration Rules and Supplementary Procedures for
+  Consumer Related Disputes in the context of Norwegian jurisdiction.)
+</p>
+<p>
+  Any arbitration award will include costs of the arbitration, reasonable
+  attorneys’ fees, and reasonable costs for witnesses. You and the company may
+  enter arbitration awards in any court with jurisdiction.
+</p>
+<h3 id="general-terms">General Terms</h3>
+<p>
+  If a provision of these terms is unenforceable as written, but could be
+  changed to make it enforceable, that provision should be modified to the
+  minimum extent necessary to make it enforceable. Otherwise, that provision
+  should be removed.
+</p>
+<p>
+  You may not assign your agreement with the company. The company may assign
+  your agreement to any affiliate of the company, any other company that obtains
+  control of the company, or any other company that buys assets of the company
+  related to the platform. Any attempted assignment against these terms has no
+  legal effect.
+</p>
+<p>
+  Neither the exercise of any right under this Agreement, nor waiver of any
+  breach of this Agreement, waives any other breach of this Agreement.
+</p>
+<p>
+  These terms are all the terms of agreement between you and the company about
+  use of the platform. These terms entirely replace any other agreements about
+  your use of the platform, written or not.
+</p>
+<h3 id="contact">Contact</h3>
+<p>
+  You may notify the company under these terms, and send questions to the
+  company, at team@roomy.space
+</p>
+<p>
+  The company may notify you under these terms using the e-mail address you
+  provide for your account on the platform, or by posting a message to the
+  homepage of the platform or your account page.
+</p>
+<h3 id="changes">Changes</h3>
+<p>
+  The company last updated these terms on September 22, 2025, and may update
+  these terms again. The company will post all updates to the platform.
+</p>
+<p>
+  For updates that contain substantial changes, the company agrees to either
+  e-mail you, if you’ve created an account and provided a valid e-mail address,
+  or to announce the updates with special messages or alerts on the platform.
+</p>
+<p>
+  Once you get notice of an update to these terms, you must agree to the new
+  terms in order to keep using the platform.
+</p>
