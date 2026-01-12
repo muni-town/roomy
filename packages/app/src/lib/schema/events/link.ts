09ab1abd835c77f8e62659106254e22d0fc9aa11
meri-leeworthy <meri.leeworthy@gmail.com>
2026-01-12 13:05:55 +1100

    fix: rename overloaded 'room' property on create/removeRoomLink

--- packages/app/src/lib/schema/events/link.ts
+++ packages/app/src/lib/schema/events/link.ts
@@ -2,12 +2,12 @@ import { type, Ulid } from "../primitives";
 
 export const CreateRoomLink = type({
   $type: "'space.roomy.link.createRoomLink.v0'",
-  room: Ulid.describe("The room to link."),
+  linkToRoom: Ulid.describe("The room to link."),
 }).describe("Inside a room, link to another room.");
 
 export const RemoveRoomLink = type({
   $type: "'space.roomy.link.removeRoomLink.v0'",
-  room: Ulid.describe("The room to unlink."),
+  linkToRoom: Ulid.describe("The room to unlink."),
 }).describe("Inside a room, unlink from another room.");
 
 export const LinkEventVariant = CreateRoomLink.or(RemoveRoomLink);
