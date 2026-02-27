a26e2d5623186a922e76f936631f85d42ae6cb80
meri-leeworthy <meri.leeworthy@gmail.com>
2026-02-27 13:10:09 +1100

    fix: center alert title

--- packages/app/src/lib/components/ui/alert/AlertTitle.svelte
+++ packages/app/src/lib/components/ui/alert/AlertTitle.svelte
@@ -22,27 +22,30 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 -->
 <script lang="ts">
-	import type { HTMLAttributes } from 'svelte/elements';
-	import type { WithElementRef } from 'bits-ui';
-	import { cn } from '$lib/utils.svelte';
+  import type { HTMLAttributes } from "svelte/elements";
+  import type { WithElementRef } from "bits-ui";
+  import { cn } from "$lib/utils.svelte";
 
-	let {
-		ref = $bindable(null),
-		class: className,
-		level = 5,
-		children,
-		...restProps
-	}: WithElementRef<HTMLAttributes<HTMLDivElement>> & {
-		level?: 1 | 2 | 3 | 4 | 5 | 6;
-	} = $props();
+  let {
+    ref = $bindable(null),
+    class: className,
+    level = 5,
+    children,
+    ...restProps
+  }: WithElementRef<HTMLAttributes<HTMLDivElement>> & {
+    level?: 1 | 2 | 3 | 4 | 5 | 6;
+  } = $props();
 </script>
 
 <div
-	role="heading"
-	aria-level={level}
-	bind:this={ref}
-	class={cn('mb-2 text-sm leading-none font-medium last:mb-0', className)}
-	{...restProps}
+  role="heading"
+  aria-level={level}
+  bind:this={ref}
+  class={cn(
+    "mb-2 py-2 text-lg leading-none font-medium last:mb-0 text-center",
+    className,
+  )}
+  {...restProps}
 >
-	{@render children?.()}
+  {@render children?.()}
 </div>
