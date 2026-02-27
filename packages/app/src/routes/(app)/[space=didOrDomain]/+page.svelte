3028ea7ef3a1cf46fbe555b9d26ec0ed7672ed67
meri-leeworthy <meri.leeworthy@gmail.com>
2026-02-27 12:36:51 +1100

    refactor: replace SpaceNotFound modal with common Error component

--- packages/app/src/routes/(app)/[space=didOrDomain]/+page.svelte
+++ packages/app/src/routes/(app)/[space=didOrDomain]/+page.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
   import { getAppState } from "$lib/queries";
-  import SpaceNotFound from "$lib/components/modals/SpaceNotFound.svelte";
+  import Error from "$lib/components/modals/Error.svelte";
   import JoinSpaceModal from "$lib/components/modals/JoinSpaceModal.svelte";
   import MainLayout from "$lib/components/layout/MainLayout.svelte";
   import { IconLoading } from "@roomy/design/icons";
+  import { page } from "$app/state";
 
   const app = getAppState();
   import { navigate } from "$lib/utils.svelte";
@@ -24,7 +25,29 @@
 
 {#if app.space.status === "error"}
   <MainLayout>
-    <SpaceNotFound message={app.space.message} />
+    <Error goHome={true} tellUs={false}>
+      {#if app.space.message}
+        <p class="text-sm text-red-700 dark:text-red-400 mb-3">
+          {app.space.message}
+        </p>
+      {/if}
+      <p class="text-sm text-base-500 dark:text-base-400 mb-3">
+        We couldn't find a space at
+        <span
+          class="font-mono bg-base-200 dark:bg-base-800 px-1.5 py-0.5 rounded"
+        >
+          {page.params.space}
+        </span>
+      </p>
+      <div class="text-sm text-base-500 dark:text-base-400 space-y-1">
+        <p>This could mean:</p>
+        <ul class="text-left list-disc list-inside ml-4">
+          <li>The space doesn't exist yet</li>
+          <li>The handle hasn't been linked to a space</li>
+          <li>The URL may be incorrect</li>
+        </ul>
+      </div>
+    </Error>
   </MainLayout>
 {:else if app.space.status === "invited"}
   <MainLayout>
