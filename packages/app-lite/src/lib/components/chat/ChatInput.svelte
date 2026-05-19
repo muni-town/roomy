c4e7e15243eb0b44a4da4418639bc7f6fef6d6db
meri-leeworthy <meri.leeworthy@gmail.com>
2026-05-20 08:32:10 +1000

    feat: port ChatInput

---
+++ packages/app-lite/src/lib/components/chat/ChatInput.svelte
@@ -0,0 +1,143 @@
+<script module lang="ts">
+  let editor: Editor;
+  export function setInputFocus() {
+    if (!editor) return;
+    editor.commands.focus();
+  }
+  export function clearInput() {
+    if (!editor) return;
+    editor.commands.clearContent();
+  }
+</script>
+
+<script lang="ts">
+  import { onMount, onDestroy } from "svelte";
+  import { Editor, Extension } from "@tiptap/core";
+  import StarterKit from "@tiptap/starter-kit";
+  import Placeholder from "@tiptap/extension-placeholder";
+  import { initUserMention, initSpaceContextMention } from "$lib/tiptap/editor";
+  import { type Item, initKeyboardShortcutHandler } from "$lib/tiptap/editor";
+  import { RichTextLink } from "$lib/tiptap/RichTextLink";
+  import { cn } from "@roomy/design/utils";
+  import { Markdown } from "tiptap-markdown";
+
+  type Props = {
+    content: string;
+    /** Users who can be mentioned with @mention */
+    users?: Item[];
+    /** Rooms in space that can be mentioned with #room */
+    context?: Item[];
+    onEnter: (content: string) => Promise<void>;
+    placeholder?: string;
+    setFocus?: boolean;
+    disabled?: boolean;
+    processImageFile?: (file: File) => void;
+  };
+
+  let {
+    content = $bindable(""),
+    users,
+    context,
+    onEnter,
+    placeholder = "Write something ...",
+    setFocus = false,
+    disabled = false,
+    processImageFile,
+  }: Props = $props();
+  let element: HTMLDivElement | undefined = $state();
+
+  let tiptap: Editor | undefined = $state();
+
+  async function wrappedOnEnter() {
+    // add one space at the end to the editor
+    tiptap?.commands.insertContent(" ");
+
+    await new Promise((resolve) => setTimeout(resolve, 10));
+
+    onEnter(content);
+  }
+
+  onMount(() => {
+    const extensions = [
+      StarterKit.configure({ heading: false }),
+      Placeholder.configure({ placeholder }),
+      RichTextLink.configure({
+        openOnClick: false,
+        autolink: true,
+        defaultProtocol: "https",
+      }),
+      initKeyboardShortcutHandler({ onEnter: wrappedOnEnter }),
+      Markdown,
+    ];
+
+    if (users) {
+      extensions.push(initUserMention({ users }) as Extension);
+    }
+    if (context) {
+      extensions.push(initSpaceContextMention({ context }) as Extension);
+    }
+
+    tiptap = new Editor({
+      element,
+      extensions,
+      content,
+      editable: !disabled,
+      editorProps: {
+        attributes: {
+          class: cn(
+            // inputVariants({ variant: "primary" }),
+            "w-full outline-none text-base-950 dark:text-base-50",
+            "max-h-[30vh] overflow-y-auto",
+          ),
+        },
+      },
+      onUpdate: (ctx) => {
+        content = ctx.editor.storage.markdown.getMarkdown();
+      },
+    });
+    editor = tiptap;
+    if (setFocus) {
+      // focus at the end of the content
+      tiptap?.commands.focus();
+      tiptap?.commands.setTextSelection({
+        from: content.length,
+        to: content.length,
+      });
+    }
+  });
+
+  $effect(() => {
+    tiptap?.setEditable(!disabled);
+    if (!disabled) setInputFocus();
+  });
+
+  onDestroy(() => {
+    tiptap?.destroy();
+  });
+
+  const handlePaste = (event: ClipboardEvent) => {
+    if (!processImageFile) return;
+
+    const items = event.clipboardData?.items;
+    if (!items) return;
+
+    for (const item of Array.from(items)) {
+      if (!item.type.startsWith("image/") && !item.type.startsWith("video/"))
+        continue;
+      const file = item.getAsFile();
+      if (!file) continue;
+      event.preventDefault();
+      processImageFile(file);
+    }
+  };
+</script>
+
+<!-- Tiptap editor -->
+<div
+  id="chat-input"
+  onpaste={handlePaste}
+  bind:this={element}
+  class="flex-1 relative"
+  role="region"
+  aria-label="Chat editor"
+></div>
